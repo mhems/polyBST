18d2df41bed33ed503743b5aba2d174c75e1768a
Matt Hems <mhems1@binghamton.edu>
2016-05-15 17:25:00 -0400

    improve create

--- create.sh
+++ create.sh
@@ -2,13 +2,17 @@
 
 function create() {
     lang=$1
-    if [ -d $lang ]; then
-        pushd . &> /dev/null
+    if [ ! -d $lang ]; then
+        mkdir $lang
         cd $lang &> /dev/null
-        touch runtests.sh Makefile
+        cat << EOF > runtests.sh
+#!/usr/bin/env bash
+
+
+EOF
         chmod +x runtests.sh
         echo "populated $lang"
-        popd &> /dev/null
+        cd ..
     fi
 }
 
